ed9784ad1e796e975fbf0da6dc755042531781e8
jmej <jmejia@anestheticaudio.com>
2026-01-22 11:41:07 -0800

    makes the moon 3d

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let phaseAnimation = 0;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400, WEBGL);
 }
 
 function draw() {
@@ -9,16 +9,21 @@ function draw() {
   phaseAnimation += 0.1;
   phaseAnimation = phaseAnimation % 30;
   for(let i = 0; i < 30; i++){
-    drawMoon(i, 50);
+    drawMoon(i, -height/2);
   }
-  drawMoon(phaseAnimation, height/2);
-  text("moonphase: "+phaseAnimation, width/2, 100);
-  fill(0);
-  ellipse(width/2, height/2, 50, 50);
+  drawMoon(phaseAnimation, 0);
+  //text("moonphase: "+phaseAnimation, width/2, 100);
+  fill(255, 0, 0);
+  ellipse(0, 0, 200, 200);
+  
 }
 
 function drawMoon(phase, h) { //phase 0-30
   fill(255, 255, 0);
-  let xLoc = map(phase, 0, 30, width/2-30, width/2+30);
-  ellipse(xLoc, h, 50, 50)
+  let xLoc = map(phase, 0, 30, -width/3, width/3);
+  console.log(xLoc);
+  push();
+  translate(xLoc, h, 0);
+  sphere(50, 6, 6);
+  pop();
 }
